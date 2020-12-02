03891a2e2189fa25a4ab117a5059803bd93f2375
Katie <katieberrym@gmail.com>
2020-12-02 15:50:45 -0800

    added transform files and initial test pass

---
+++ transform.test.js
@@ -0,0 +1,9 @@
+const fs = require('fs').promises;
+const { transform } = require('./transform');
+
+describe('transform fn', () => {
+    it('read a file, remove all capital letters, then make all letters capital, then reverse the string', async () => {
+
+
+    })
+});

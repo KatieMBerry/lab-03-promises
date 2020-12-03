53f909e656eb51bbe688b9e298947050c4867365
Katie <katieberrym@gmail.com>
2020-12-02 19:49:02 -0800

    added getCharacter file and test file; initial test pass

---
+++ rickAndMortyApi.js
@@ -0,0 +1,8 @@
+const fetch = require('node-fetch');
+
+async function getCharacter(id) {
+
+
+}
+
+module.export = { getCharacter };
---
+++ rickAndMortyApi.test.js
@@ -0,0 +1,22 @@
+const { getCharacter } = require('./rickAndMortyApi')
+
+describe('getCharater fn', () => {
+    it('takes in an id and a promise and returns a character object in the correct format', async () => {
+
+
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+/*getCharacter that takes an id and returns a promise that resolves to a character
+return only the characters name, status, and species
+WRITE YOUR TESTS FIRST*/

447aa47c8b131edb31e0600a5c350a9a60a5eb47
Katie <katieberrym@gmail.com>
2020-12-02 20:02:13 -0800

    getCharacter function test pass

--- rickAndMortyApi.js
+++ rickAndMortyApi.js
@@ -1,8 +1,21 @@
 const fetch = require('node-fetch');
 
 async function getCharacter(id) {
-
-
+    try {
+        return fetch(`https://rickandmortyapi.com/api/character/${id}`)
+            .then(res => res.json())
+            .then(({
+                name,
+                status,
+                species
+            }) => ({
+                name,
+                status,
+                species
+            }))
+    } catch (err) {
+        console.log(err)
+    }
 }
 
-module.export = { getCharacter };
+module.exports = { getCharacter };
--- rickAndMortyApi.test.js
+++ rickAndMortyApi.test.js
@@ -1,10 +1,18 @@
-const { getCharacter } = require('./rickAndMortyApi')
+const { getCharacter } = require('./rickAndMortyApi');
+// const fetch = require('node-fetch');
 
 describe('getCharater fn', () => {
     it('takes in an id and a promise and returns a character object in the correct format', async () => {
 
-
-
+        charId = 3
+        return getCharacter(charId)
+            .then(object => {
+                expect(object).toEqual({
+                    name: "Summer Smith",
+                    status: "Alive",
+                    species: "Human"
+                });
+            })
     })
 })
 

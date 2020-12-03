8097d389b9eb069edcf630b164e0c264f82b5e16
Katie <katieberrym@gmail.com>
2020-12-02 17:19:13 -0800

    transform test pass oy!

--- transform.js
+++ transform.js
@@ -1,19 +1,18 @@
 const fs = require('fs').promises;
 
-
 async function transform(src) {
-    // try {
-    //     const fileData = await fs.readFile(src, 'utf-8');
-    //     await fs.writeFile(dst, fileData);
-
-    // } catch (err) {
-    //     console.log(err);
-    // }
-}
+    try {
+        return await fs.readFile(src, 'utf-8')
+            .then(fileData => fileData.split('').filter(filtered => filtered === filtered.toLowerCase()))
+            .then(fileData => fileData.map(item => item.toUpperCase()))
+            .then(fileData => fileData.reverse().join(''))
+    } catch (err) {
+        console.log(err)
+    }
+};
 
 module.exports = { transform };
 
-/*use fsPromises.readFile to read a file
-then remove all capital letters
-then make all letters capital
-then reverse the string*/
+
+
+
--- transform.test.js
+++ transform.test.js
@@ -2,8 +2,12 @@ const fs = require('fs').promises;
 const { transform } = require('./transform');
 
 describe('transform fn', () => {
-    it('read a file, remove all capital letters, then make all letters capital, then reverse the string', async () => {
+    it('reads a file, remove all capital letters, then make all letters capital, then reverse the string', async () => {
 
-
-    })
-});
+        return await transform('./newFile.txt')
+            .then(
+                transformedFile => expect(transformedFile)
+                    .toEqual('SIHT TUOBA ERUS TON MA '))
+    }
+    )
+})

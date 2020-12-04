00aef3c9b83eed83c4368180ddbe33b71f484d09
Katie <katieberrym@gmail.com>
2020-12-04 14:51:00 -0800

    added mock - test pass

--- rickAndMortyApi.test.js
+++ rickAndMortyApi.test.js
@@ -1,14 +1,19 @@
 const { getCharacter, getManyCharacters } = require('./rickAndMortyApi');
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
+const mockResponse = require('./mock-response.json');
+
+jest.mock('node-fetch');
 
 describe('getCharater fn', () => {
     it('takes in an id and a promise and returns a character object in the correct format', async () => {
-
+        fetch.mockResolvedValue({
+            json: () => Promise.resolve(mockResponse)
+        });
         charId = 3
         return getCharacter(charId)
             .then(object => {
                 expect(object).toEqual({
-                    name: "Summer Smith",
+                    name: "Morty Smith",
                     status: "Alive",
                     species: "Human"
                 });
@@ -18,12 +23,15 @@ describe('getCharater fn', () => {
 
 describe('getManyCharacters fn', () => {
     it('that takes an array of ids and returns a promise that resolves with an array of characters', async () => {
-        const characters = await getManyCharacters([1, 3, 5])
+        fetch.mockResolvedValue({
+            json: () => Promise.resolve(mockResponse)
+        });
+        const characters = await getManyCharacters([1, 2, 3])
 
         expect(characters).toEqual([
-            { name: "Rick Sanchez", status: "Alive", species: "Human" },
-            { name: "Summer Smith", status: "Alive", species: "Human" },
-            { name: "Jerry Smith", status: "Alive", species: "Human" }
+            { name: "Morty Smith", status: "Alive", species: "Human" },
+            { name: "Morty Smith", status: "Alive", species: "Human" },
+            { name: "Morty Smith", status: "Alive", species: "Human" }
         ])
     })
 })
